57bd7465098fb0f605bffeb6fda8d5c939756251
Kevin Yao <ksyao2002@gmail.com>
2020-09-11 00:48:57 -0500

    init

---
+++ Assets/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public AudioSource explain1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void playAudio()
+    {
+        if (!explain1.isPlaying)
+        {
+            explain1.Play();
+        }
+    }
+
+    public void record()
+    {
+        RecordVideo();
+    }
+
+    private void RecordVideo()
+    {
+        NativeCamera.Permission permission = NativeCamera.RecordVideo((path) =>
+        {
+            UnityEngine.Debug.Log("Video path: " + path + " is the path");
+            if (path != null)
+            {
+                // Play the recorded video
+                Handheld.PlayFullScreenMovie("file://" + path);
+            }
+        }, NativeCamera.Quality.Medium, 100, 100L, NativeCamera.PreferredCamera.Front);
+
+        UnityEngine.Debug.Log("Permission result: " + permission);
+    }
+}
